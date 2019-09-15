9bf2a2d9dbbd049750671463d367f4b87351e8e2
ultraspace2029 <galaxi1031@gmail.com>
2019-09-15 15:59:39 +0900

    結合バージョン

---
+++ project/Assets/Script/Capture/Bone3.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bone3 : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float x = 0f;
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            //Debug.Log (GameObject.Find("bone.003").transform.position);
+            Vector3 axis1 = Vector3.right;
+            Quaternion q1 = Quaternion.AngleAxis(1, axis1);
+            this.transform.rotation = this.transform.rotation * q1;
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            //Debug.Log (Vector3.forward);
+            Vector3 axis1 = Vector3.right;
+            Quaternion q1 = Quaternion.AngleAxis(-1, axis1);
+            this.transform.rotation = this.transform.rotation * q1;           
+        }
+    }
+}
---
+++ project/Assets/Script/Capture/IKtest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKtest : MonoBehaviour
+{
+    Animator anim;
+    float reach=0;
+    void Start () {
+    anim= GetComponent<Animator>();
+    }
+    void OnAnimatorIK() {
+    anim.SetIKPositionWeight(AvatarIKGoal.RightHand, reach);
+    anim.SetIKPosition(AvatarIKGoal.RightHand, transform.position+new Vector3(0,3,2) );
+}
+
+    // Update is called once per frame
+    void Update () {
+	if(  Input.GetKeyUp( KeyCode.RightArrow ) )
+		reach+=0.02f;
+	if(  Input.GetKeyUp( KeyCode.LeftArrow ) )
+		reach-=0.02f;
+}
+}
